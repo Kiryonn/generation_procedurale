64b31d4d481740044d37e09d0510704265caaf43
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-07 00:30:03 +0100

    {ADD} Email Data Script

---
+++ Assets/Scripts/Data/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+    public class Email
+    {
+        public string address;
+        public string header;
+        public string body;
+        public string footer;
+    }
+}
