d9c5bd86ef6961ae329d214f9b6b6e1097b9f716
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-03-06 10:49:02 +0100

    bar de Reputation

---
+++ Assets/Scripts/ReputionBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// ReputionBar sert a changée la valeur de la barre affichée 
+/// </summary>
+public class ReputionBar : MonoBehaviour
+{
+    public float reputation = 0.3f;
+    public Image reputationFill;
+
+    /// <summary>
+    /// change la valeur de la barre de reputaion pour une valeur x 
+    /// </summary>
+    /// <param name="x">taille de la barre entre 0 et 1 en float </param>
+    void SetRepution(float x)
+    {
+        reputation = x;
+        if (reputation > 1) { reputation = 1; }
+        if (reputation < 0) { reputation = 0; }
+        reputationFill.transform.localScale = new Vector3( reputation,1,1);
+        
+    }
+}
