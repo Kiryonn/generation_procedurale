ca2e592458cdd3ebe8379034354f5d82860a5301
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-08 19:58:33 +0100

    {Add} Data structure to load e-mails data

---
+++ Assets/Scripts/Data/EmailBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data
+{
+	[Serializable]
+	public class EmailBlock
+	{
+		public Information addresses;
+		public Information headers;
+		public Information bodies;
+		public Information footers;
+	}
+}
---
+++ Assets/Scripts/Data/Information.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data
+{
+	[Serializable]
+	public class Information
+	{
+		public List<string> valid;
+		public List<string> invalid;
+	}
+}
