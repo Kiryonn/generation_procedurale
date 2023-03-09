16bbd3277eb02653c83cc6af6326f0402777c33d
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-03-09 10:29:14 +0100

    menuPricipale

---
+++ Assets/Scripts/loaderScenes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loaderScenes : MonoBehaviour
+{
+    
+
+        public void LoadGame()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+        public void LoadMenuPrincipale()
+    {
+        SceneManager.LoadScene("menuPrincipale");
+    }
+}
