898eb76db2421d15ac9d171ebfb122cf9c46917c
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-03-09 12:21:44 +0100

    menu session  script

--- Assets/Scripts/loaderScenes.cs
+++ Assets/Scripts/loaderScenes.cs
@@ -7,13 +7,33 @@ public class loaderScenes : MonoBehaviour
 {
     
 
+    public void LoadGame(int n)
+    {
+        PlayerPrefs.SetInt("Session", n);
+        SceneManager.LoadScene("game");
+        //Debug.Log(PlayerPrefs.GetInt("Session"));
+    }
+    
+
         public void LoadGame()
     {
+        PlayerPrefs.SetInt("Session", 1);
         SceneManager.LoadScene("game");
     }
-     
+
         public void LoadMenuPrincipale()
     {
         SceneManager.LoadScene("menuPrincipale");
     }
+
+        public void LoadMenuSession()
+    {
+        SceneManager.LoadScene("MenuSession");
+    }
+
+    public void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+
 }
