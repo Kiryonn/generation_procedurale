38ed208bd4379de5e5b9c50147859e613c891d80
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-10 18:37:45 +0100

    Added a Draggable Script for draggable objects

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+	[NonSerialized] public RectTransform rectTransform;
+	[NonSerialized] public Canvas canvas;
+	[NonSerialized] public Vector2 originalPosition;
+
+	private void Awake()
+	{
+		rectTransform = GetComponent<RectTransform>();
+		canvas = GetComponentInParent<Canvas>();
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		originalPosition = rectTransform.anchoredPosition;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+	}
+}
