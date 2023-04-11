3308f430b42b31454dd84c30dddcd3f4917d747e
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-04-11 15:32:53 +0200

    add document prefab+ fonctionnalité + structure   modif de game

---
+++ Assets/Scripts/Data/DocumentStrutuDonne.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DocumentTechniqueAttaque")]
+public class DocumentStrutuDonne : ScriptableObject
+{
+    public List<DocumentInformation> typeDeTechnique;
+}
+
+[System.Serializable]
+public class DocumentInformation
+{
+    public string Titre;
+    public string text;
+}
---
+++ Assets/Scripts/UI/Documentation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Documentation : MonoBehaviour
+{
+    public TextMeshProUGUI titre;
+    public TextMeshProUGUI text;
+    public DocumentStrutuDonne ficheTec;
+    public Button Left;
+    public Button Right;
+    int ficherActuel;
+    private void Start()
+    {
+        aficher(ficherActuel);
+    }
+    public void right() 
+    {
+        aficher(ficherActuel + 1);
+    }
+    public void left() 
+    {
+        aficher(ficherActuel - 1);
+    }
+
+    void aficher(int n)
+    {
+        ficherActuel = n;
+        titre.text=ficheTec.typeDeTechnique[ficherActuel].Titre;
+        text.text = ficheTec.typeDeTechnique[ficherActuel].text;
+        afficheButon();
+    }
+
+    void afficheButon() 
+    {
+        if(ficherActuel== ficheTec.typeDeTechnique.Count-1) { Right.interactable = false; }
+        else { Right.interactable = true; }
+
+        if (ficherActuel == 0) { Left.interactable = false; }
+        else { Left.interactable = true; }
+    }
+}
