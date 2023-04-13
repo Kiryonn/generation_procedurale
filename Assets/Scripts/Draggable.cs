dc0978f95bca9b8ca67b861f3a5b69933c64543b
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-04-13 19:26:52 +0200

    made dragable properties protected instead of public

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler
 {
-	[NonSerialized] public RectTransform rectTransform;
-	[NonSerialized] public Canvas canvas;
-	[NonSerialized] public Vector2 originalPosition;
+	protected RectTransform rectTransform;
+	protected Canvas canvas;
+	protected Vector2 originalPosition;
 
-	private void Start()
-	{
+	private void Start() {
 		rectTransform = GetComponent<RectTransform>();
 		canvas = GetComponentInParent<Canvas>();
 	}
 
-	public void OnBeginDrag(PointerEventData eventData)
-	{
+	public void OnBeginDrag(PointerEventData eventData) {
 		originalPosition = rectTransform.anchoredPosition;
 	}
 
-	public void OnDrag(PointerEventData eventData)
-	{
+	public void OnDrag(PointerEventData eventData) {
 		rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
 	}
 }
