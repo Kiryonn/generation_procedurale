2b3d1a98f4ee80c428bf0e3d0173d3748fb94aa6
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-04-13 19:27:22 +0200

    made "Hache" herit from Dragable

--- Assets/Scripts/Hache.cs
+++ Assets/Scripts/Hache.cs
@@ -1,58 +1,13 @@
-using Data;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class Hache : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class Hache : Draggable, IEndDragHandler {
+	[SerializeField] private RectTransform[] dropableAreas;
 
-    [SerializeField]
-    private Canvas canvas;
-    //public Image devant;
-    private Vector2 basePosition;
-    //public RectTransform rectTransformToCheck;
-
-
-    private void Start()
-    {
-        basePosition = transform.position;
-
-    }
-
-    public void DragHandler(BaseEventData data)
-    {
-        //devant.gameObject.SetActive(true);
-
-        PointerEventData pointerData = (PointerEventData)data;
-        Vector2 position;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-        (RectTransform)canvas.transform, pointerData.position, canvas.worldCamera, out position);
-
-        transform.position = canvas.transform.TransformPoint(position);
-    }
-
-    public void DropHandler(BaseEventData data)
-    {
-        /*devant.gameObject.SetActive(false);
-
-        PointerEventData eventData = (PointerEventData)data;
-
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            rectTransformToCheck,
-            eventData.position, 
-            eventData.pressEventCamera, 
-            out Vector2 localPoint);
-
-        if (rectTransformToCheck.rect.Contains(localPoint))
-        {
-            //Debug.Log("mail valid�e");
-        }*/
-
-
-
-        transform.position = basePosition;
-    }
+	public void OnEndDrag(PointerEventData eventData) {
+		if (dropableAreas.Any(area => RectTransformUtility.RectangleContainsScreenPoint(area, eventData.position, canvas.worldCamera)))
+			return;
+		rectTransform.anchoredPosition = originalPosition;
+	}
 }
