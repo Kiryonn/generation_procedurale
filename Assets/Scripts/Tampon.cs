5d1b7aeba95c4e0a4f5f43af41ac6bc0de6dbad8
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-04-14 11:05:08 +0200

    tampon suit le pointer au centre

--- Assets/Scripts/Tampon.cs
+++ Assets/Scripts/Tampon.cs
@@ -13,12 +13,23 @@ public class Tampon: Draggable, IEndDragHandler
 		base.OnBeginDrag(eventData);
 		top.gameObject.SetActive(true);
 		front.gameObject.SetActive(false);
-	}
+
+
+
+        Vector2 localMousePosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(top.rectTransform.parent as RectTransform, eventData.position, null, out localMousePosition);
+
+        // Centrer l'objet "top" sur la position de la souris
+        top.rectTransform.localPosition = localMousePosition;
+
+    }
 
 	public void OnEndDrag(PointerEventData eventData) {
-		top.gameObject.SetActive(false);
+        Debug.Log("x : " + eventData.position.x + " y : " + eventData.position.y);
+        top.gameObject.SetActive(false);
 		front.gameObject.SetActive(true);
-
+		
+			
 		RectTransformUtility.ScreenPointToLocalPointInRectangle(
 			rectTransformToCheck,
 			eventData.position,
