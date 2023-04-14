b7d9a1f8dccf989240bf8e355988aafaa62adfcd
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-04-14 10:00:17 +0200

    Update Tampon.cs
    - Made Tampon herit from dragable

--- Assets/Scripts/Tampon.cs
+++ Assets/Scripts/Tampon.cs
@@ -1,79 +1,33 @@
-using Data;
 using Managers;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Tampon : MonoBehaviour
+public class Tampon: Draggable, IEndDragHandler
 {
-    // Start is called before the first frame update
+	[SerializeField] private Image top;
+	[SerializeField] private Image front;
+	public RectTransform rectTransformToCheck;
 
-    [SerializeField]
-    private Canvas canvas;
-    public Image top;
-    public Image front;
-    private Vector2 basePosition;
-    public RectTransform rectTransformToCheck;
-    //public GameManager GameManager;
+	public override void OnBeginDrag(PointerEventData eventData) {
+		base.OnBeginDrag(eventData);
+		top.gameObject.SetActive(true);
+		front.gameObject.SetActive(false);
+	}
 
+	public void OnEndDrag(PointerEventData eventData) {
+		top.gameObject.SetActive(false);
+		front.gameObject.SetActive(true);
 
-    private void Start()
-    {
-        basePosition = transform.position;
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(
+			rectTransformToCheck,
+			eventData.position,
+			eventData.pressEventCamera,
+			out Vector2 localPoint);
 
-    }
+		if (rectTransformToCheck.rect.Contains(localPoint))
+			GameManager.Instance.mailValider(true);
 
-    public void DragHandler(BaseEventData data)
-    {
-        top.gameObject.SetActive(true);
-        front.gameObject.SetActive(false);
-
-        PointerEventData pointerData = (PointerEventData)data;
-        Vector2 position;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-        (RectTransform)canvas.transform, pointerData.position, canvas.worldCamera, out position);
-
-        transform.position = canvas.transform.TransformPoint(position);
-    }
-
-    public void DropHandler(BaseEventData data)
-    {
-        top.gameObject.SetActive(false);
-        front.gameObject.SetActive(true);
-         
-
-        /*
-
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-        (RectTransform)canvas.transform,
-        ((PointerEventData)data).position,
-        canvas.worldCamera,
-        out Vector2 position);
-
-        Debug.Log("position: " + position.x);
-        var isEmailVisible = rightArea.rect.Contains(position);*/
-
-        PointerEventData eventData = (PointerEventData)data;
-
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            rectTransformToCheck,
-            eventData.position, 
-            eventData.pressEventCamera, 
-            out Vector2 localPoint);
-
-        if (rectTransformToCheck.rect.Contains(localPoint))
-        {
-            //appel de la fonction de l'aparition de la cire
-            GameManager.Instance.mailValider(true);
-        }
-
-        
-
-        
-        transform.position = basePosition;
-
-       
-    }
+		transform.position = originalPosition;
+	}
 }
