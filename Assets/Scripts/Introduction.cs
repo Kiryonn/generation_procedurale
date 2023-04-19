0ece8a34b9d3985bc5205f524cef60124a0f9060
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-04-19 16:32:40 +0200

    mind blowed. First try baby

--- Assets/Scripts/Introduction.cs
+++ Assets/Scripts/Introduction.cs
@@ -1,58 +1,51 @@
+// Namespace imports
 using Managers;
+
+// Unity imports
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Introduction : MonoBehaviour
-{
-    private float _counter;
+public class Introduction: MonoBehaviour {
+	private float _counter;
+	[SerializeField] private TextMeshProUGUI _rulesText;
+	[SerializeField] private TextMeshProUGUI Titre;
+	[SerializeField] private float time;
 
-    [SerializeField] private TextMeshProUGUI _rulesText;
-    [SerializeField] private TextMeshProUGUI Titre;
+	private void Update() {
+		_counter += Time.deltaTime;
+		if (_counter > time)
+			end();
+	}
 
-    [SerializeField] private float time;
+	private void end() {
+		gameObject.SetActive(false);
+	}
 
-    void Update()
-    {
-        _counter += Time.deltaTime;
-        if (_counter > time)
-        {
-            end();
-        }
-    }
+	public void Restart(Rules rules, int curentDay) {
+		_counter = 0;
+		Titre.text = "Jour      :  " + curentDay;
 
-    private void end()
-    {
-        gameObject.SetActive(false);
-        GameManager.Instance.Go();
-    }
+		// Construire le texte Ã  afficher en fonction des rï¿½gles actives
+		string rulesActiveText = "RÃ¨gles actives : \n";
+		if (rules.HasFlag(Rules.InvalidAddress))
+			rulesActiveText += "Il y a de Mauvaise adresse email \n";
+		if (rules.HasFlag(Rules.IncorrectSpelling))
+			rulesActiveText += "Il y a des faute d'orthographe \n"; //todo
+		if (rules.HasFlag(Rules.PersonalData))
+			rulesActiveText += "Il y a des demandes de donnï¿½es personnelles \n"; //todo
+		if (rules.HasFlag(Rules.FishyLink))
+			rulesActiveText += "Il y a des lien piÃ¨gÃ© \n";
+		if (rules.HasFlag(Rules.ExaggeratedMail))
+			rulesActiveText += "Il y a des mails faisant appel aux sentiments\n";
+		if (rules.HasFlag(Rules.WeirdHeader))
+			rulesActiveText += "Il y a des mails avec titre de mail douteux\n";
+		if (rules.HasFlag(Rules.Threat))
+			rulesActiveText += "Il y a des Mail contenant des menace\n";
+		// Afficher le texte des rÃ¨gles actives
+		if (rulesActiveText.Length < 30)
+			rulesActiveText += "pas de regles actives";
 
-    public void Restart(Rules rules, int curentDay)
-    {
-        _counter = 0;
-        Titre.text = "Jour      :  " + curentDay;
-
-        // Construire le texte à afficher en fonction des règles actives
-        string rulesActiveText = "Règles actives : \n";
-        if (rules.HasFlag(Rules.InvalidAddress))
-            rulesActiveText += "Il y a de Mauvaise adresse email \n";
-        if (rules.HasFlag(Rules.IncorrectSpelling))
-            rulesActiveText += "Il y a des faute d'orthographe \n";//todo
-        if (rules.HasFlag(Rules.PersonalData))
-            rulesActiveText += "Il y a des demandes de données personnelles \n";//todo
-        if (rules.HasFlag(Rules.FishyLink))
-            rulesActiveText += "Il y a des lien piégé \n";
-        if (rules.HasFlag(Rules.ExaggeratedMail))
-            rulesActiveText += "Il y a des mails faisant appel aux sentiments\n";
-        if (rules.HasFlag(Rules.WeirdHeader))
-            rulesActiveText += "Il y a des mails avec titre de mail douteux\n";
-        if (rules.HasFlag(Rules.Threat))
-            rulesActiveText += "Il y a des Mail contenant des menace\n";
-
-        // Afficher le texte des règles actives
-        if (rulesActiveText.Length < 30) { rulesActiveText += "pas de regles actives"; }
-        _rulesText.text = rulesActiveText;
-
-
-    }
+		_rulesText.text = rulesActiveText;
+	}
 }
